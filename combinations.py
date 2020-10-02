005cbc042c945d5df7a4717c89a99c8f7ffe8f6a
Kawika Brennan <32173874+kawikabrennan@users.noreply.github.com>
2020-10-02 13:16:34 -0700

    Added the code to make the screening test pass
    I admit I cheated a bit. I had to look up something that would combine the list because I couldn't figure out how to get recursion working. There has to be a way to do it, but I just couldn't get zip() to work as I thought it would and I was getting into too much nesting that caused performance issues.

--- combinations.py
+++ combinations.py
@@ -1,2 +1,18 @@
-def print_sum_combinations():
-    return True
+from itertools import combinations
+
+
+def print_sum_combinations(numbers: list, specified_sum: int):
+    results = []
+
+    if specified_sum != 0:
+        for number in range(len(numbers)):
+            for combination in combinations(enumerate(numbers), number):
+                sum_of_values = 0
+                for _, value in combination:
+                    sum_of_values += value
+                if sum_of_values == specified_sum:
+                    result = " ".join([str(index[0]) for index in combination])
+                    results.append(result)
+                    print(result)
+
+    return results
--- test_combinations.py
+++ test_combinations.py
@@ -4,5 +4,19 @@ from combinations import print_sum_combinations
 
 class CombinationsTests(unittest.TestCase):
 
-    def test_initial(self):
-        self.assertEqual(True, print_sum_combinations())
+    def test_zero_ignored(self):
+        self.assertEqual([], print_sum_combinations([1], 0))
+
+    def test_prints_indicies_equals_4(self):
+        numbers = [1, 1, 2, 2, 4]
+        self.assertEqual(
+            ["0 1 2", "0 1 3", "2 3", "4"],
+            sorted(print_sum_combinations(numbers, 4))
+        )
+
+    def test_prints_indicies_equals_6(self):
+        numbers = [1, 1, 2, 7, 12]
+        self.assertEqual(
+            [],
+            sorted(print_sum_combinations(numbers, 6))
+        )
